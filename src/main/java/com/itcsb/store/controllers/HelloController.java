a52a42e39ec5006f8dd43cdd05017b188d0a33cd
Essam <essam.elmanser@gmail.com>
2019-12-02 14:49:08 +0100

    add angular config files

--- src/main/java/com/itcsb/store/controllers/HelloController.java
+++ src/main/java/com/itcsb/store/controllers/HelloController.java
@@ -1,12 +1,17 @@
 package com.itcsb.store.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 public class HelloController {
-    @RequestMapping("/")
-    public String hello() {
-        return "hello world";
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
     }
+
+
 }
